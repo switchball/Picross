438b77acbd49add78f16fe0615eaf6178aeb19b2
Art9 <xyc.nju@gmail.com>
2014-11-15 18:59:15 +0800

    wrap Disc with Maybe, and substitute zipWith with zipWithM

--- picross.hs
+++ picross.hs
@@ -1,4 +1,5 @@
 import Data.List (transpose, intersperse)
+import Control.Monad
 
 nest :: (t -> t) -> t -> Int -> t
 nest f x n
@@ -25,18 +26,22 @@ buildseq ns x y = concat $ intersperse [y] (map (\n->build n x) ns)
 data Disc = Un|Oc|Va
             deriving (Show,Read,Eq)
 
-discAnd :: Disc -> Disc -> Disc
-discAnd x Un = x
-discAnd Un x = x
-discAnd Oc Oc = Oc
-discAnd Va Va = Va
-discAnd Oc Va = error "Oc and Va = Contradiction!"
-discAnd Va Oc = error "Va and Oc = Contradiction!"
+discAnd :: Disc -> Disc -> Maybe Disc
+--discAnd Nothing _ = Nothing
+--discAnd _ Nothing = Nothing
+discAnd Un x = Just x
+discAnd x Un = Just x
+discAnd Oc Oc = Just Oc
+discAnd Va Va = Just Va
+discAnd Oc Va = Nothing
+discAnd Va Oc = Nothing
 
-discOr :: Disc -> Disc -> Disc
-discOr Oc Oc = Oc
-discOr Va Va = Va
-discOr _  _  = Un
+discOr :: Disc -> Disc -> Maybe Disc
+--discOr Nothing x = x
+--discOr x Nothing = x
+discOr Oc Oc = Just Oc
+discOr Va Va = Just Va
+discOr _  _  = Just Un
 
 createDisc :: Int -> Int -> [[Disc]]
 createDisc m = take m . repeat . createDisc1d
@@ -50,7 +55,10 @@ createSeq = id
 type Sequence = [Int]
 type Sequences = [[Int]]
 
-seq0 = createSeq [0]
+seq0   = createSeq [0]
+seq3   = createSeq [3]
+seq5   = createSeq [5]
+seq111 = createSeq [1,1,1]
 seqs1  = [[1],[1],[5],[1],[1]]
 seqs2 = [[1],[1],[5],[1],[1]]
 
@@ -63,24 +71,26 @@ noname (v,h) = createDisc (length h) (length v)
 uncertainty :: [[Disc]] -> Int
 uncertainty = count2d Un 
 
-applyRule :: Sequence -> [Disc] -> [Disc]
-applyRule []  ds = zipWith discAnd ds (build n Va) where n = length ds
+applyRule :: Sequence -> [Disc] -> Maybe [Disc]
+applyRule []  ds = zipWithM discAnd ds (build n Va) where n = length ds
 applyRule [m] ds
-  | m == 0  = zipWith discAnd ds (build n Va)
-  | m == n  = zipWith discAnd ds (build n Oc)
-  | 2*m > n = zipWith discAnd ds (build (n-m) Un ++ build (2*m-n) Oc ++ build (n-m) Un ) 
+  | m == 0  = zipWithM discAnd ds (build n Va)
+  | m == n  = zipWithM discAnd ds (build n Oc)
+  | 2*m > n = zipWithM discAnd ds (build (n-m) Un ++ build (2*m-n) Oc ++ build (n-m) Un ) 
     where n = length ds
 applyRule seq ds
-  | sum seq + length seq - 1 == n = zipWith discAnd ds (buildseq seq Oc Va) where n = length ds
+  | sum seq + length seq - 1 == n = zipWithM discAnd ds (buildseq seq Oc Va) where n = length ds
 
 
-partialFill :: ([Sequence], [[Disc]]) -> [[Disc]]
+partialFill :: ([Sequence], [[Disc]]) -> [Maybe [Disc]]
 partialFill = uncurry . zipWith $ applyRule
 
-reduceState :: ([Sequence], [[Disc]]) -> ([Sequence], [[Disc]])
+reduceState :: ([Sequence], [[Disc]]) -> ([Sequence], [Maybe [Disc]])
 reduceState x = ((fst x), partialFill x)
 
+{-
 solve ((hr,g):(vr,gt):[]) 
   | uncertainty g == 0 = g
   | otherwise          = solve $ [(vr, transpose . snd $ result), result] 
                             where result = reduceState (hr,g)
+-}
